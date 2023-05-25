e5112519119db1322061216322fccf9cd3a25b1a
rylee <ryfrogquinn@gmail.com>
2023-05-25 11:35:28 -0500

    started on routine routes

---
+++ routes/routines.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { getAllPublicRoutines } = require("../db/adapters/routines");
+const { createRoutine, destroyRoutine } = require("../db/adapters/routines");
+const { getRoutineById } = require("../db/adapters/routines");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  const publicRoutines = await getAllPublicRoutines();
+  res.send({
+    status: 200,
+    status_message: "got all public routines",
+    data: publicRoutines,
+  });
+});
+
+router.post("/", async (req, res, next) => {
+  const { creatorId, isPublic, name, goal } = req.body;
+  const newRoutine = await createRoutine(creatorId, isPublic, name, goal);
+  res.send({
+    status: 200,
+    status_message: "created routine",
+    data: newRoutine,
+  });
+});
+
+router.delete("/:routineId", async (req, res, next) => {
+  const { routineId } = req.params;
+  const routine = await destroyRoutine(routineId);
+  if (!routine) throw error;
+  res.send({
+    status: 200,
+    status_message: "deleted routine",
+    data: null,
+  });
+});
+
+module.exports = router;
